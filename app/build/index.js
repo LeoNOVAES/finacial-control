10fdd3fbe39ab25b3293af792863065ba4ffdf4a
Leandro Novaes <leandrofern.novaes@gmail.com>
2022-02-27 18:39:32 +0000

    back some next execution date stuffs

--- app/build/index.js
+++ app/build/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var mongoose_1 = __importDefault(require("mongoose"));
-var routes_1 = __importDefault(require("./transation/routes"));
+var routes_1 = __importDefault(require("../transation/routes"));
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(routes_1.default);
